aa2ba39ef0c3e3c78759b60266b58e7738ff5da1
Maykel Matheus Dias <maykel.winchester@hotmail.com>
2019-08-28 10:25:56 -0300

    adjust

--- src/screens/ExampleScreen/index.js
+++ src/screens/ExampleScreen/index.js
@@ -5,17 +5,22 @@ import { Container } from '~/components';
 import styles from './styles';
 import { colors } from '~/styles';
 
-export default function MainScreen() {
-  return (
-    <Container style={styles.container}>
-      <StatusBar backgroundColor={colors.purpleDark} barStyle="light-content" />
-      <Text style={styles.welcome}>Bem vindo</Text>
-      <Text style={styles.description}>
-        Para começar é necessário configurar:
-      </Text>
-      <Text style={styles.item}>
-        ■ Reactotron Debug: adb reverse tcp:9090 tcp:9090
-      </Text>
-    </Container>
-  );
-}
+const MainScreen = () => {
+    return (
+        <Container style={styles.container}>
+            <StatusBar
+                backgroundColor={colors.purpleDark}
+                barStyle="light-content"
+            />
+            <Text style={styles.welcome}>Bem vindo</Text>
+            <Text style={styles.description}>
+                Para começar é necessário configurar:
+            </Text>
+            <Text style={styles.item}>
+                ■ Reactotron Debug: adb reverse tcp:9090 tcp:9090
+            </Text>
+        </Container>
+    );
+};
+
+export default MainScreen;
